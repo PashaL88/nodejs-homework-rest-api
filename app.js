f7aea1ada1b194292d458c9a46512f8ab6dbf84b
Pavel Lobtsov <plobtsov@gmail.com>
2022-07-08 13:31:10 +0300

    database added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// DbMepGYsBkETaiRO
-// ov7wob9dRIyYFVrH   = Node-project
 const express = require("express");
 // const logger = require("morgan");
 const cors = require("cors");
@@ -13,6 +11,16 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
+
 // const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 // app.use(logger(formatsLogger));
@@ -25,7 +33,5 @@ app.use((error, req, res, next) => {
   const { status = 500, message = "Server error" } = error;
   res.status(status).json({ message });
 });
-
-// app.listen(3000, console.log("Runing"));
 
 module.exports = app;
