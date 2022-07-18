835a9822204186aad285b53fa698b75f3c92eb36
Pavel Lobtsov <plobtsov@gmail.com>
2022-07-18 21:58:38 +0300

    Begin h/w4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
