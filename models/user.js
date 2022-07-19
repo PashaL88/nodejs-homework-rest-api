831cfe47e2a9bffe6bd080b12dc0d73880dd9eda
Pavel Lobtsov <plobtsov@gmail.com>
2022-07-19 12:14:18 +0300

    signup added

--- models/user.js
+++ models/user.js
@@ -1,6 +1,23 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = Schema({}, { versionKey: false, timestamps: true });
+const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const User = model("user", userSchema);
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const Joi = require("joi");
 
 const router = express.Router();
 
+const User = require("../../models/user");
+
+const { createError } = require("../../helpers/");
+
+const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+const userRegisterSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post("/signup", async (req, res, next) => {
   try {
+    const { error } = userRegisterSchema.validate(req.body);
+    if (error) {
+      throw createError(400, "Ошибка от Joi или другой библиотеки валидации");
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw createError(409, "Email in use");
+    }
+    const result = await User.create({ email, password });
+    res.status(201).json({ email: result.email });
   } catch (error) {
     next(error);
   }
